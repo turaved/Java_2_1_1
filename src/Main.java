18a3ba2728c6c4c89888d513ffd50593db77030c
Alex Turovets <madlilu177@gmail.com>
2020-11-21 12:12:50 +0300

    added code

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,8 @@
 public class Main {
+    public static void main(String[] args) {
+        int TicketCost = 80000;
+        int MilesCounter = TicketCost/20;
+        System.out.print("Bonus Miles = ");
+        System.out.println(MilesCounter);
+    }
 }
